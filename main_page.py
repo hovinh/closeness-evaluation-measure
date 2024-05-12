e546daa8340972371ef6acca399e43737656c665
hovinh <hovinh39@gmail.com>
2024-05-12 17:18:17 +0800

    add: introduction

--- main_page.py
+++ main_page.py
@@ -15,26 +15,85 @@ from src.viz_utils import (
 )
 
 st.set_page_config(
-    page_title="",
+    page_title="Closeness Evaluation Measure",
     page_icon="📊",
     layout="centered",
     menu_items={
         "About": "# this is a header. This is a cool app!",
     },
 )
+
 st.markdown("# An Ordinal Classification Metric: Closeness Evaluation Measure")
+st.markdown(
+    """
+    [Original Blog](url) |
+    [Github](https://github.com/hovinh/closeness-evaluation-measure) |
+    [Cited Paper](https://aclanthology.org/2020.acl-main.363.pdf)
+    """
+)
 
 st.markdown("## Introduction")
-st.write("Conventionally, we typically ordinal classification tasks a")
-st.write("Relative within the class itself")
+description = """
+**Ordinal classification** is a type of classification task whose predicted classes (or categories) have a specific ordering.
+This implies misclassifying a class pair should be penalized differently and proportionally, with respect to their semantic order and class distribution in the dataset.
+Existing evaluation metrics are shown failed to capture certain aspects of this task, that it calls for the employment of a new metric, namely **Closeness Evaluation Measure** (**$CEM$**).
 
-st.markdown("## An Ordinal Classification Metrics")
-st.write("Content for Section 1 goes here.")
-st.write("The measure is not symmetrical.")
+To be clear,
+- precision/recall ignores the ordering among classes.
+- mean average error/mean squared error assumes a predefined interval between classes, subject to their numeric encoding.
+- ranking measures indeed capture the relative ranking in terms of relevancy, but lack of emphasis on identifying the correct class.
+
+$CEM$ employs the idea of informational closeness: the more **UNLIKELY** there is data point between a class pair, the closer they are.
+As a result, a model that misclassifies informational-close classes should be penalized lesser than those informational-distant.
+It has been mathematically proven this measure attains the desired properties that addresses the foregoing shortcomings, which to be discussed shortly after.
+
+This demo is divided as follows:
+- [Informational Closeness](#informational-closeness)
+- [Closeness Evaluation Measure](#closeness-evaluation-measure)
+- [Metric Properties](#metric-properties)
+- [An Example Usecase](#an-example-usecase)
+"""
+st.markdown(description)
+
+st.markdown("## Informational Closeness", unsafe_allow_html=True)
+description = """
+Let's say we have two journals $F$ and $S$ (mockup data below) with two very distinct paper review methods.
+Reviewers at journal $F$ are very cautious and it's rare to see they review a paper as hard *reject* or hard *accept*.
+Journal $S$, on the other hand, tends to take a clear stance on the paper's acceptance/rejection status.
+
+Given that, *weak reject* vs. *weak accept* in the context of $S$ are informational close because they are closer assessment, while
+$F$ treating them as two far ends of the grading scales due to the fact this is a strong disagreement between reviewers in the context of $F$.
+Put it differently, two classes $a$ and $b$ are informationally close if the probability of finding data points between the two is low.
+
+More formally,
+"""
+st.markdown(description)
+
+html_code = """
+<div style="display: flex;">
+    <div style="flex: 1; border-right: 1px solid black; padding-right: 10px; text-align: center;">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>Sed euismod, sem vel dapibus aliquam, velit nisi dictum mi, vel bibendum nisl nulla non leo.
+    </div>
+    <div style="flex: 1; padding-left: 10px;text-align: center;">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sem vel dapibus aliquam, velit nisi dictum mi, vel bibendum nisl nulla non leo.
+    </div>
+</div>
+"""
+st.markdown(html_code, unsafe_allow_html=True)
+
+description = """
+Formally, 
+$F$'s   
+Taking the 
+The measure is not symmetrical.
+Formula
+"""
+st.markdown(description)
+
 with st.form("paper_review_dist_form"):
     col1, col2 = st.columns(2, gap="small")
     with col1:
-        st.markdown("### First distribution")
+        st.markdown("### Journal $F$")
         first_review_confusion_matrix_df = pd.read_csv(
             "data/paper-review-example-01.csv", header=0
         )
@@ -43,7 +102,7 @@ with st.form("paper_review_dist_form"):
         )
 
     with col2:
-        st.markdown("### Second distribution")
+        st.markdown("### Journal $S$")
         second_review_confusion_matrix_df = pd.read_csv(
             "data/paper-review-example-02.csv", header=0
         )
@@ -123,11 +182,11 @@ with col2:
                     )
 
 
-st.markdown("## CEM definition")
+st.markdown("## Closeness Evaluation Measure", unsafe_allow_html=True)
 st.write("Something")
 
 # Section 2
-st.markdown("## Metric Properties")
+st.markdown("## Metric Properties", unsafe_allow_html=True)
 st.write("Content for Section 2 goes here.")
 # Showing characteristics of metrics
 st.markdown("### Ordinal Invariance")
@@ -249,7 +308,8 @@ with col2:
         )
 
 # Section 3
-st.markdown("## Section 3")
+st.markdown("## An Example Usecase", unsafe_allow_html=True)
+
 # showing the comparisoin between 2 distribution with the same accuracy
 
 with st.container(border=True):
